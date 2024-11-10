1127b1c1f3df6d6798ff3a044263fd28c52cb6e8
Joe <pavlovks2004@yandex.ru>
2024-11-10 06:43:32 +0300

    some deplot fixes

--- new_back/main.py
+++ new_back/main.py
@@ -5,7 +5,41 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from views import router as views_router
 from config import settings
-import full_pipeline
+from transformers import AutoModel
+import torch
+import torch.nn as nn
+
+class BertMultiRegressor(nn.Module):
+    """
+    Модель мульти регрессора на основе эмбеддингов BERT
+    """
+    def __init__(self, bert_model_name, output_size):
+        super(BertMultiRegressor, self).__init__()
+        self.bert = AutoModel.from_pretrained(bert_model_name)
+        self.regressor = nn.Linear(self.bert.config.hidden_size + 7, output_size)
+
+    def forward(self,
+                input_ids,
+                attention_mask=None,
+                audio_traits=None):
+
+        # Текстовые данные обрабатываются в BERT
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+        )
+
+        # Берём [CLS] токен для получения эмбеддингов текста
+        last_hidden_state = outputs.last_hidden_state[:, 0, :]
+
+        # Объединяем числовые данные с эмбеддингами текста
+        result = torch.concat((last_hidden_state, audio_traits), dim=1)
+
+        outputs = self.regressor(result).to(torch.float64)
+
+        return {
+            "logits": outputs
+        }
 
 app = FastAPI()
 
@@ -16,7 +50,7 @@ origins = [
     "https://213.171.28.36:5173",
     "http://localhost:5173",
     "https://localhost:5173",
-    # "*"
+    "*"
 ]
 
 app.add_middleware(
