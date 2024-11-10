0ccff3074d6ac210f927dea9d1c386682c7525b6
Joe <pavlovks2004@yandex.ru>
2024-11-10 05:54:20 +0300

    some deplot fixes

--- new_back/config.py
+++ new_back/config.py
@@ -2,13 +2,13 @@ from pathlib import Path
 from pydantic import BaseModel
 import os
 
+WEIGHTS_PATH = "hope_bert"
+
 if os.environ.get("mode", "dev") == 'prod':
     DB_PATH = "/static/db/db.sqlite3"
     FAISS_INDEX_PATH = "/static/db/vacancy_index.index"
-    WEIGHTS_PATH = "hope_bert"
 else:
     DB_PATH = "./db.sqlite3"
-    WEIGHTS_PATH = "weights"
 
 print(DB_PATH)
 
