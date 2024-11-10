486d00404a2a6e9f20996224340e87aaabd3d1b3
Joe <pavlovks2004@yandex.ru>
2024-11-10 06:57:52 +0300

    some deplot fixes

--- new_back/full_pipeline.py
+++ new_back/full_pipeline.py
@@ -356,3 +356,41 @@ def ocean_to_holland_codes(data):
 # caption = generate_features(video_path)
 # print(caption)
 # print("Время выполнения", time.time() - start_time)
+
+
+
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from views import router as views_router
+from config import settings
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=settings.static_dir_path), name="static")
+
+origins = [
+    "http://213.171.28.36:5173",
+    "https://213.171.28.36:5173",
+    "http://localhost:5173",
+    "https://localhost:5173",
+    # "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(views_router)
+
+
+@app.get("/")
+def hello():
+    return {"hello": "world"}
+
